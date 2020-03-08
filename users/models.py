af25e9879ec8f85b8e5c18727a818edfc3a18039
Tianqi Wang <twang2@ualberta.ca>
2020-03-08 12:08:58 -0600

    auto generate Display name

--- users/models.py
+++ users/models.py
@@ -13,6 +13,21 @@ class User(AbstractUser):
     host = models.URLField(blank=True, null=True, default="127.0.0.1:8000")  # FIXME HARDCODED HOST
     firstName = models.CharField(max_length=20, null=True, blank=True)
     lastName = models.CharField(max_length=20, null=True, blank=True)
-    displayName = models.CharField(max_length=40, null=True, blank=True, default="{} {}".format(str(firstName), str(lastName)))
+    displayName = models.CharField(max_length=40, null=True, blank=True)
     github = models.URLField(blank=True, null=True,)
+
+    def save(self, *args, **kwargs):
+        self.generate_display_name()
+        super(User, self).save(*args, **kwargs)
+
+    def generate_display_name(self):
+        if not self.displayName:
+            if self.firstName and self.lastName:
+                self.displayName = "{} {}".format(self.firstName, self.lastName)
+            elif self.firstName:
+                self.displayName = self.firstName
+            elif self.lastName:
+                self.displayName = self.lastName
+            else:
+                self.displayName = self.id
 
