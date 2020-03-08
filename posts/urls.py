d1abd34abf4037a57c919a8adea4cf9d88669515
Kaixiang Zhang <kaixiangzhang@Kaixiangs-MacBook-Pro.local>
2020-03-08 16:14:37 -0600

    fix backend

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from posts import views
+
+urlpatterns = [
+    path('posts/author/<str:author_id>', views.get_posts_author),
+    path('posts/user/<str:author_id>', views.get_posts_author),
+    #path('snippets/<int:pk>', views.snippet_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
