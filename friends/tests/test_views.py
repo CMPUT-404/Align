695fbc36a61cf924e0490a01f38265572f76d45d
aoladosu <oladosu@ualberta.ca>
2020-03-08 18:12:47 -0600

    - test views

--- friends/tests/test_views.py
+++ friends/tests/test_views.py
@@ -4,8 +4,35 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from ..models import Friends, Followers, FriendRequests
 from ..serializers import FriendsSerializer, FollowersSerializer, FriendRequestSerializer
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
-# initialize the APIClient app
-client = Client()
+class FriendRequestAPITest(APITestCase):
+    """ Test module for GET all puppies API """
+    
+    def test_get_all_following(self):
+        url = "/following/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_all_friendrequests(self):
+        url = "/friendrequest/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_all_friends(self):
+        url = "/friend/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_all_author(self):
+        url = "/author/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_all_author(self):
+        url = "/author/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+         
 
 
