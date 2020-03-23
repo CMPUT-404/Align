8a07326b9b0373748003bd87fd6090adff8bfeae
KaixiangCS <kaixiang@ualberta.ca>
2020-03-23 13:05:44 -0700

    trying to fix heroku problem #2

--- posts/models.py
+++ posts/models.py
@@ -18,6 +18,7 @@ class Posts(models.Model):
         'web',
         'tutorial'
     ]
+	#visibility = models.BooleanField(default = True)
 	visibility = models.TextField(blank=True,default = "PUBLIC",max_length=300)
 	visibleTo = models.TextField(blank=True,max_length=300)
 	image = models.TextField(blank=True,max_length=300)
